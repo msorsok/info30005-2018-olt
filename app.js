087407647f3cc24af41c101294c1b06c6a204e49
Mina Sorsok <mina.sorsok@gmail.com>
2018-03-30 15:15:47 +1100

    added 404 handler

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
+
 app.use('/', router);
+
+app.get('*', function(req, res){
+    res.status(404).send('Oops you took a wrong turn.');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){

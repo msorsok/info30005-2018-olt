e50f0712d823b6a339cdc74efdac6e7f29307a04
Luis <lajero@student.unimelb.edu.au>
2018-04-18 17:49:17 +1000

    added account and newmessage

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -11,11 +11,20 @@ const loginRoute = function(req, res) {
 
 const blankRoute = function(req, res) {
     res.render('page_template');
+}
+const accountRoute = function(req, res) {
+    res.render('account');
+};
+
+const newMessageRoute = function(req, res) {
+    res.render('newmessage');
 };
 
 
 module.exports = {
     comingSoonRoute: comingSoonRoute,
     loginRoute: loginRoute,
-    blankRoute: blankRoute
+    blankRoute: blankRoute,
+    accountRoute: accountRoute,
+    newMessageRoute: newMessageRoute
 };
--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,6 @@ var router = express.Router();
 router.get('/', controller.comingSoonRoute);
 router.get("/login", controller.loginRoute );
 router.get("/blank", controller.blankRoute);
+router.get("/account", controller.accountRoute);
+router.get("/newmessage", controller.newMessageRoute);
 module.exports = router;

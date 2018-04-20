3b8a7f50a39fca3dba256059f269de6d82989a3c
Luis <lajero@student.unimelb.edu.au>
2018-04-20 15:46:21 +1000

    edited account.ejs and account.js

--- public/js/account.js
+++ public/js/account.js
@@ -1,5 +1,27 @@
-var editNameForm = document.getElementById("editNameForm").addEventListener("click",editNameFunc);
 
-function editNameFunc(e) {
-    alert("form processed");
+/*add functionality for changing name for user*/
+
+function mouseClick(link) {
+    /*
+    var editFirstName = document.getElementById("firstName").valueOf();
+
+    var currName = document.getElementsByClassName("userName");
+    var currNamePTag = currName.getElementsByTagName("p")[0];
+    if (editFirstName != null && editLastName != null) {
+        currNamePTag.innerHTML = editFirstName;
+    } else {
+        currNamePTag.innerHTML = "uh oh!"
+    }*/
+
+    return function() {
+        window.location.href = link;
+    }
+}
+
+/* getElementsByClassName returns a list of all elements withing the class sidebar
+*  els[i] is the i'th element in that list (the i'th element in class submitAccountChanges
+*  children returns a list of all the elements of children*/
+var els = document.getElementsByClassName("submitAccountChanges")[0].children;
+for (var i = 0 ; i < els.length; i++) {
+    els[i].addEventListener("click", mouseClick("account"));
 }

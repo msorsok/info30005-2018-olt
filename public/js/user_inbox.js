c7c4c9cb1e1823ad79618a753af28f944cef4b07
Tarnvir Grewal <tarnvirgrewal@gmail.com>
2018-04-20 15:41:04 +1000

    Updating user_inbox

--- public/js/user_inbox.js
+++ public/js/user_inbox.js
@@ -1,18 +1,10 @@
-/*function mouseOver() {
-    document.getElementsByClassName("Account").children.style.backgroundColor = "#888888";
-    console.log("worked");
-}
-*/
-
 function mouseOver(el) {
     return  function() {
-        console.log(el.childNodes);
-        console.log(Node.ELEMENT_NODE);
-        el.style.backgroundColor = "#888888";
+        el.style.backgroundColor = "#56327d";
         for (var i = 0 ; i < el.childNodes.length; i++) {
             if (el.childNodes[i].nodeType == Node.ELEMENT_NODE) {
                 console.log(el.childNodes[i]);
-                el.childNodes[i].style.backgroundColor = "#888888";
+                el.childNodes[i].style.backgroundColor = "#56327d";
             }
         }
     }
@@ -20,29 +12,29 @@ function mouseOver(el) {
 
 function mouseOut(el) {
     return  function() {
-        console.log(el.childNodes);
-        console.log(Node.ELEMENT_NODE);
-        el.style.backgroundColor = "#f1f1f1";
+        el.style.backgroundColor = "#957ead";
         for (var i = 0 ; i < el.childNodes.length; i++) {
             if (el.childNodes[i].nodeType == Node.ELEMENT_NODE) {
                 console.log(el.childNodes[i]);
-                el.childNodes[i].style.backgroundColor = "#f1f1f1";
+                el.childNodes[i].style.backgroundColor = "#957ead";
             }
         }
     }
 }
 
+function mouseClick(link) {
+    return function() {
+        window.location.href = link;
+    }
+}
+
 
 var els = document.getElementsByClassName("sidebar")[0].children;
-console.log(els);
 for (var i = 0 ; i < els.length; i++) {
-    console.log(els[i]);
     els[i].addEventListener("mouseover", mouseOver(els[i]));
     els[i].addEventListener("mouseout", mouseOut(els[i]));
-
+    els[i].addEventListener("click", mouseClick(els[i].className));
 }
 
 
 
-
-//document.getElementsByClassName("Account").addEventListener("mouseout", mouseOut);

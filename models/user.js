8a54ca4b840aa76c2fe7140efc9a19baa616a6df
Luis <lajero@student.unimelb.edu.au>
2018-05-07 14:49:44 +1000

    created Schema object

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-
+require("./capsule.js");
+var Schema = mongoose.Schema;
 var userSchema =  mongoose.Schema(
     {
         "firstName" : String,
@@ -7,11 +8,11 @@ var userSchema =  mongoose.Schema(
         "DOB" : Date,
         "password": String,
         "email" : String,
-        "capsules": {type: Schema.ObjectId, ref: 'capsule'},
+        "capsules": {type: Schema.ObjectId, ref: 'Capsule'},
         "profilePic" : {data: Buffer, contentType: String},
         "nominee1email" : String,
         "nominee2email" : String
     }
 );
 
-mongoose.model('user', userSchema);
+mongoose.model('User', userSchema);

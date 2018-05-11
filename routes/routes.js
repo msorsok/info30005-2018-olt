42476e462fb0013b61726c54f042c541e09c8448
Luis <lajero@student.unimelb.edu.au>
2018-05-11 13:04:35 +1000

    safety commit

--- routes/routes.js
+++ routes/routes.js
@@ -2,28 +2,105 @@ var express = require("express");
 var controller = require('../controllers/controllers');
 var router = express.Router();
 var passport = require("passport");
-router.get('/', controller.loginRoute);
-router.get("/blank", controller.blankRoute);
-router.get("/unlock", controller.unlockRoute);
-router.get("/account", controller.accountRoute);
-router.get("/create", controller.createRoute);
-router.get("/userWelcome", controller.userWelcomeRoute);
-router.get("/userInbox",controller.userInboxRoute);
-router.get("/account2", controller.account2Route);
-router.get("/view/:id", controller.viewRoute);
 
-router.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/profile', // redirect to the secure profile section
-    failureRedirect : '/signup', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-}));
+/*
+module.exports = function(passport) {
+    router.get('/', controller.loginRoute);
+    router.get("/blank", controller.blankRoute);
+    router.get("/unlock", controller.unlockRoute);
+    router.get("/account", controller.accountRoute);
+    router.get("/create", controller.createRoute);
+    router.get("/userWelcome", controller.userWelcomeRoute);
+    router.get("/userInbox",controller.userInboxRoute);
+    router.get("/account2", controller.account2Route);
+    router.get("/view/:id", controller.viewRoute);
 
-router.post("/loginUser", controller.userLogin);
+    router.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
-router.post("/create", controller.createCapsule);
-router.post("/unlock", controller.unlockCapsule);
-router.post("/account", controller.updateAccount);
+    router.post("/loginUser", controller.userLogin);
+
+    router.post("/create", controller.createCapsule);
+    router.post("/unlock", controller.unlockCapsule);
+    router.post("/account", controller.updateAccount);
+
+    return router;
+}*/
 
 
+/*
+ // app/routes.js
+module.exports = function(app, passport) {
 
-module.exports = router;
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        res.render('index.ejs'); // load the index.ejs file
+    });
+
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
+ *
+ */
+
+
+// module.exports = router;
